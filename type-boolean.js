1358250fca30569408bcfbe092a9a370ab8a0d42
Leonardo Gabriel Polido <136330641+LeonadoGPolido@users.noreply.github.com>
2023-08-23 08:44:40 -0300

    Update type-boolean.js

--- type-boolean.js
+++ type-boolean.js
@@ -1,3 +1,5 @@
 const numeroPrimeiro = 5;
 const numeroSegundo = 10;
-console.log(numeroPrimeiro === numeroSegundo);
+console.log(numeroPrimeiro === numeroSegundo); => False
+
+//Nestes casos, pode haver o retorno de true ou false em seu console.
