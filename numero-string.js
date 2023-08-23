ce1e74deaf65c363e1ce410075bf77428e980e9c
Leonardo Gabriel Polido <136330641+LeonadoGPolido@users.noreply.github.com>
2023-08-23 09:18:52 -0300

    Update and rename Index.js to numero-string.js

--- numero-string.js
+++ numero-string.js
@@ -2,11 +2,11 @@ const primeiroNumero = 3;
 const segundoNumero = 2;
 
 const numeroFlutuante = 2.2;
-const numeroFlutuanteSemZero = .5;
+const numeroFlutuanteSemZero = .5; //0.5
 
 // Existe também o Nan (Not a number), quando se utiliza operações matemáticas sem um número (NÃO É UM ERRO)
 
 const citacao = "Meu nome é "
 const meuNome = "Leonardo"
 
-console.log(citacao + meuNome);
+//console.log(citacao + meuNome); => Meu nome é Leonardo (Concatenação)
